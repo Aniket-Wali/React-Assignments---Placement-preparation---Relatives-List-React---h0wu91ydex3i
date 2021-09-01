f6f42d6924d34a719d6d5afeaf9edd9248d475a5
Aniket Waliyan <30599754+Aniket-Wali@users.noreply.github.com>
2021-09-01 14:30:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
-import React, {Component, useState} from "react";
+ import React, {Component, useState} from "react";
 import '../styles/App.css';
+
+const relativeList = ['abc','abc1','abc3','abc4'];
 
 class App extends Component {
     render() {
@@ -7,6 +9,12 @@ class App extends Component {
         return(
             <div id="main">
                {/* Do not remove the main div */}
+               <ol key='relativeList'>
+               {relativeList.map((item,i) =>{
+                return <li key ={relativeListItem${i+1}}>{item}</li>
+                
+               })}
+               </ol>
             </div>
         )
     }
